0b2c1bbde503765c4a105845bfdb080f25b0e460
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-16 23:31:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // Yahin sirf link change karna hai jab server ka URL badle
-const MEDIA_SERVER_URL = "https://tapes-franchise-daughter-workout.trycloudflare.com";
+const MEDIA_SERVER_URL = "https://biz-arise-iron-analytical.trycloudflare.com";
+
