fe725c13a3e9383f0f52f839e6834166413d1fba
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-16 22:12:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const content = document.getElementById("content");
 
-// Example: server se data load karna
-fetch(MEDIA_SERVER_URL)
-  .then(res => res.text())
-  .then(data => {
-    content.innerHTML = data;
-  })
-  .catch(() => {
-    content.innerHTML = "Server offline hai ya link galat hai.";
-  });
+content.innerHTML = `
+  <iframe 
+    src="${MEDIA_SERVER_URL}" 
+    style="width:100%; height:90vh; border:none; border-radius:12px; background:#000;">
+  </iframe>
+`;
