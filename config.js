45439f31463dbb036958c5cbd1271434aef0f795
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-17 20:07:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Yahin sirf link change karna hai jab server ka URL badle
-const MEDIA_SERVER_URL = "https://biz-arise-iron-analytical.trycloudflare.com";
+const MEDIA_SERVER_URL = "https://competitors-success-training-broke.trycloudflare.com";
+
 
