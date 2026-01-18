745089ba9fcf40677b4ca01b88591d7c08e1d5e3
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-18 14:00:20 +0530

    Update MEDIA_SERVER_URL to new server address

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Yahin sirf link change karna hai jab server ka URL badle
-const MEDIA_SERVER_URL = "https://competitors-success-training-broke.trycloudflare.com";
+const MEDIA_SERVER_URL = "https://additionally-candy-injured-heart.trycloudflare.com";
+
 
 
