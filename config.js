d70956f1ebc00f571521aecb4f80287c85860abd
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-27 21:13:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // Yahin sirf link change karna hai jab server ka URL badle
-const MEDIA_SERVER_URL = "https://anne-preceding-generating-geneva.trycloudflare.com";
+const MEDIA_SERVER_URL = "https://opposed-cool-worm-straight.trycloudflare.com";
+
 
 
 
